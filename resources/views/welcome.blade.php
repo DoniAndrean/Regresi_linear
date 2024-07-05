34c7ba6cbe8cf78b10ed02ce7d507ad15c3f56e2
flutisholic <flutisholic15@gmail.com>
2024-07-05 12:20:05 +0700

    fix: home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,21 +72,7 @@
                         </div>
                     </div>
                     <!-- ./col -->
-                    <div class="col-lg-3 col-6">
-                        <!-- small box -->
-                        <div class="small-box bg-warning">
-                            <div class="inner">
-                                <h3>{{ $totalJadwalPelatihan }}</h3>
-
-                                <p>Jadwal Pelatihan</p>
-                            </div>
-                            <div class="icon">
-                                <i class="ion ion-person"></i>
-                            </div>
-                            <a href="/jadwal-pelatihan" class="small-box-footer">More info <i
-                                    class="fas fa-arrow-circle-right"></i></a>
-                        </div>
-                    </div>
+                    
                     <!-- ./col -->
                     <div class="col-lg-3 col-6">
                         <!-- small box -->
